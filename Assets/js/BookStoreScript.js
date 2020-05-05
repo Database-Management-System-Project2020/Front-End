ad9b2f1a080c2bcc44c40d174ec2f246faabb1c5
israa-salah <israasalahyousef97@gmail.com>
2020-05-05 09:58:40 +0200

    product section

--- Assets/js/BookStoreScript.js
+++ Assets/js/BookStoreScript.js
@@ -1,3 +1,43 @@
 $(document).ready(function(){ 
-    
+    // navbar 
+    var a = document.querySelectorAll("nav .navbar-nav .nav-item a")
+    a.forEach(element => {
+        var linkAttrValue = element.getAttribute("link")
+        element.addEventListener("click", function(e){
+            var x = e.target.getAttribute("link")
+            document.getElementById(x).style.display = "block";    
+        })
+    }); 
+
+    // filtering products
+    $("#products-Input").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#products-table tbody tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+     
+    // filtering recurring profiles 
+    $("#recurring-Input").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#recurring-table tbody tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+    // filtering customers 
+    $("#customer-Input").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#customer-table tbody tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+    // filtering suppliers 
+    $("#supplier-Input").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#supplier-table tbody tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
 }); 
